922940443265571baa5f91aa26e46df7de94a881
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2026-04-11 19:15:21 +0200

    implemented iterative correction scheme

--- compse3/composite_transformation.py
+++ compse3/composite_transformation.py
@@ -568,7 +568,7 @@ class SE3CompositeTransform:
         
         if correction:
 
-            for i in range(5):
+            for i in range(10):
                 # Step 3: First-order ground state calculation
                 M_R_inv = np.linalg.inv(M_R)
                 alpha_fo = -M_R_inv @ b
